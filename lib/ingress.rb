c38a72d2e904c68f29e0680822657113c2beaf19
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-11-06 13:06:24 +0000

    Update annotations only if the key exists. If not create and update

--- lib/ingress.rb
+++ lib/ingress.rb
@@ -16,6 +16,8 @@ class Ingress
 
     # Set the new ingress class and name
     m = ingress.fetch("metadata")
+
+    m["annotations"] = {} unless m.has_key?("annotations")
     m.fetch("annotations")["kubernetes.io/ingress.class"] = ingress_class
     m["name"] = ingress_name
 
@@ -29,7 +31,7 @@ class Ingress
     ingress.delete("status")
     m = ingress.fetch("metadata")
     %w( creationTimestamp generation resourceVersion selfLink uid).each { |key| m.delete(key) }
-    m.fetch("annotations").delete("kubectl.kubernetes.io/last-applied-configuration")
+    m.fetch("annotations").delete("kubectl.kubernetes.io/last-applied-configuration") if m.has_key?("annotations")
   end
 
   def kube_deploy_json(json)
