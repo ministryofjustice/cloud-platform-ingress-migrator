281002adfb595026811a1c5297894ce9c1809c11
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-12-16 16:58:32 +0000

    Refactor the script to change if conditions and use array, filter and map.

--- bin/list-nginx-class-ingresses.rb
+++ bin/list-nginx-class-ingresses.rb
@@ -2,7 +2,9 @@
 
 # Script to build the list of ingresses and its namespaces with
 # the ingress-class annotations as "nginx" or empty i.e "" or ingress
-# with no annotations and into a json file
+# with no annotations and into a json file 
+# This script will list ingress for all non-prod namespaces. For production ingresses
+# change if non_production?(namespace) to if production?(namespace)
 
 require "json"
 require "open3"
@@ -10,41 +12,33 @@ require "open3"
 NGINX_CLASS_INGRESS_LIST_FILE = "nginx_class_ingresses.json"
 
 def main
-  nginx_class_ingresses = list_ingresses_has_annotations("nginx")
-  null_class_ingresses = list_ingresses_has_annotations("")
-  no_annotation_ingresses = list_ingresses_no_annotations
-  list = nginx_class_ingresses + null_class_ingresses + no_annotation_ingresses
+  $namespaces = get_namespaces
+  nginx_class_ingresses = ingresses_matching_class("nginx")
+  null_class_ingresses = ingresses_matching_class("")
+  no_annotation_ingresses = ingresses_matching_no_class
 
-  puts "Total ingress listed "
-  puts list.size
+  list = nginx_class_ingresses.compact + null_class_ingresses.compact + no_annotation_ingresses.compact
+  
+  puts "Total ingress listed: #{list.size}"
+
   File.write(NGINX_CLASS_INGRESS_LIST_FILE, JSON.pretty_generate(list))
 end
 
-def list_ingresses_has_annotations(target_ingress_class)
+def ingresses_matching_class(target_ingress_class)
   ingress_array = []
-  get_ingresses.map do |ingress|
-    if ingress.dig("metadata").include?("annotations")
-      if ingress.dig("metadata", "annotations", "kubernetes.io/ingress.class").to_s == target_ingress_class
-        ingress_name = ingress.dig("metadata", "name")
-        namespace = ingress.dig("metadata", "namespace")
-        if check_is_production(namespace).include?("false")
-          ingress_array.push({namespace: namespace, ingress_name: ingress_name})
-        end
-      end
-    end
+  get_ingresses
+  .filter { |ingress| ingress.dig("metadata", "annotations", "kubernetes.io/ingress.class").to_s == target_ingress_class }
+  .map do |ingress|
+      ingress_array.push(non_production_tuple(ingress))
   end
   ingress_array
 end
 
-def list_ingresses_no_annotations
+def ingresses_matching_no_class
   ingress_array = []
   get_ingresses.map do |ingress|
     unless ingress.dig("metadata").include?("annotations")
-      ingress_name = ingress.dig("metadata", "name")
-      namespace = ingress.dig("metadata", "namespace")
-      if check_is_production(namespace).include?("false")
-        ingress_array.push({namespace: namespace, ingress_name: ingress_name})
-      end
+        ingress_array.push(non_production_tuple(ingress))
     end
   end
   ingress_array
@@ -62,15 +56,27 @@ def get_ingresses
   JSON.parse(stdout).fetch("items")
 end
 
-def check_is_production(namespace)
-  cmd = "kubectl get ns #{namespace} -o json | jq '[.metadata.labels[\"cloud-platform.justice.gov.uk/is-production\"]]'"
-
-  stdout, stderr, status = Open3.capture3(cmd)
-
-  unless status.success?
-    raise stderr
+def non_production_tuple(ingress)
+  ingress_name = ingress.dig("metadata", "name")
+  namespace = ingress.dig("metadata", "namespace")
+  if non_production?(namespace)
+    {namespace: namespace, ingress_name: ingress_name}
   end
-  JSON.parse(stdout)[0]
+end
+
+def get_namespaces
+  json = `kubectl get namespaces -o json`
+  JSON.parse(json).fetch("items")
+end
+
+def non_production?(namespace)
+  ns = $namespaces.find { |n| n.dig("metadata", "name") == namespace }
+  ns.dig("metadata", "labels", "cloud-platform.justice.gov.uk/is-production") == "false"
+end
+
+def production?(namespace)
+  ns = $namespaces.find { |n| n.dig("metadata", "name") == namespace }
+  ns.dig("metadata", "labels", "cloud-platform.justice.gov.uk/is-production") == "true"
 end
 
 ############################################################
